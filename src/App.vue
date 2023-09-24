f57b28bf1a6b8f2fd66deecc643395425e0ce1c3
Issam Athamena <aymenissamathamena@gmail.com>
2023-09-24 04:25:20 +0100

    everything put together only design touches left

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,33 @@
 <template>
-  <div id="app">
-    <Navbar />
-    <RandomUserList />
+  <div class="relative flex flex-col">
+    <div class="top-0 fixed left-0 w-screen z-10 mb-6">
+      <Navbar />
+    </div>
+
+    <div class="mt-20 flex w-screen justify-between items-center container mx-auto h-14">
+      <div class="text-4xl">
+        100 Contact(s)
+      </div>
+      <div class="w-40v h-full">
+        <SearchField />
+      </div>
+    </div>
+
+    <div class="my-10 mt-4">
+      <RandomUserList />
+    </div>
   </div>
 </template>
 
 <script>
 import RandomUserList from './components/RandomUserList.vue';
 import Navbar from './components/Navbar.vue';
+import SearchField from './components/SearchField.vue';
 
 export default {
   components: {
     Navbar,
+    SearchField,
     RandomUserList,
   },
 };
---
+++ src/components/SearchField.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex items-center border border-slate-700 rounded-md overflow-hidden h-full">
+    <input
+      type="text"
+      placeholder="Search..."
+      class="py-2 px-4 w-64 focus:outline-none"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  // Vue component options go here
+};
+</script>
+
+<style scoped>
+/* Add any scoped styles here */
+</style>
